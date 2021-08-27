83dcaf07ff87a1d4780121d39fd8accb03ab8c4a
Christopher Robinson <chrispaul_robinson@hotmail.com>
2021-08-27 05:02:45 -0500

    Complete base alignment, add base css

--- frontend-app/src/components/OrderSummary.jsx
+++ frontend-app/src/components/OrderSummary.jsx
@@ -3,19 +3,22 @@ import React, { useState } from "react";
 import Card from "./Card";
 
 const OrderWrap = styled.div`
+  /* TODO: Add background image */
   /* Layout */
   display: flex;
   max-width: 90%;
   height: 100vh;
 
   align-items: center;
+  justify-content: center;
 
   margin-left: auto;
   margin-right: auto;
   padding-left: 1rem;
   padding-right: 1rem;
 
-  @media (min-width: 1170px) {
+  /* TODO: Complete Responsiveness */
+  @media (min-width: 1440px) {
     max-width: 1170px;
   }
 
